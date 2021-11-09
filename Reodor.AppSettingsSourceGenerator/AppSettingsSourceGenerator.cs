f55a83773605853711e55515fc0e626416303891
frodehus <frode.hus@outlook.com>
2021-11-09 22:33:52 +0100

    refactored to netstandard2.0

--- Reodor.AppSettingsSourceGenerator/AppSettingsSourceGenerator.cs
+++ Reodor.AppSettingsSourceGenerator/AppSettingsSourceGenerator.cs
@@ -1,120 +1,147 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
-using System.Text.Json;
 
-namespace Reodor.AppSettingsSourceGenerator;
 
-[Generator]
-public class AppSettingsSourceGenerator : ISourceGenerator
+namespace Reodor.AppSettingsSourceGenerator
 {
 
-    internal static string GenerateRecordSource(string recordName, Dictionary<string, object> values)
+    [Generator]
+    public class AppSettingsSourceGenerator : ISourceGenerator
     {
-        var builder = new StringBuilder();
-        builder.Append(@"
+
+        internal static string GenerateRecordSource(string recordName, Dictionary<string, object> values)
+        {
+            var builder = new StringBuilder();
+            builder.Append(@"
 using System;
-namespace AppSettings;
+namespace AppSettings
+{
 
 ");
-        builder
-            .Append("public record ")
-            .AppendLine(recordName)
-            .Append('{')
-            .AppendLine();
-        if (values[recordName] == null || values[recordName] is not JsonElement)
-        {
-            throw new ArgumentException($"No values found in given dictionary for {recordName}", nameof(values));
-        }
-
-        var propValues = (JsonElement)values[recordName];
-        var properties = propValues.ValueKind switch
-        {
-            JsonValueKind.Object => GeneratePropertiesFromDict(propValues.Deserialize<Dictionary<string, object>>() ?? new Dictionary<string, object>()),
-            _ => null
-        };
-
-        if (properties != null)
-        {
-            builder.Append(properties);
-        }
-
-        builder.AppendLine("}");
-        return builder.ToString().Trim();
-    }
-
-    internal static string GeneratePropertiesFromDict(Dictionary<string, object> values)
-    {
-        var builder = new StringBuilder();
-        foreach (var kvp in values)
-        {
-            var declaration = GetPropertyTypeAndName(kvp.Key, kvp.Value);
-            builder.Append("    public ").Append(declaration).AppendLine(" { get; init; }");
-        }
-        return builder.ToString();
-    }
-
-    internal static List<string> ExtractTopLevelPropertyNames(Dictionary<string, object> values)
-    {
-
-        if (values == null)
-        {
-            return new List<string>();
-        }
-
-        return values.Select(x => StringToValidRecordName(x.Key)).ToList();
-    }
-
-
-    internal static string GetPropertyTypeAndName(string fieldName, object fieldValue)
-    {
-        var typeName = fieldValue switch
-        {
-            true => "bool",
-            false => "bool",
-            int _ => "int",
-            double _ => "double",
-            float _ => "float",
-            _ => "string"
-        };
-        return $"{typeName} {fieldName}";
-    }
-
-    public void Execute(GeneratorExecutionContext context)
-    {
-        foreach (var file in context.AdditionalFiles)
-        {
-            if (!Path.GetExtension(file.Path).Equals(".json", StringComparison.OrdinalIgnoreCase)
-                || !file.Path.StartsWith("appsettings", StringComparison.OrdinalIgnoreCase))
+            builder
+                .Append("    public record ")
+                .AppendLine(recordName)
+                .Append('{')
+                .AppendLine();
+            if (values[recordName] == null)
             {
-                continue;
+                throw new ArgumentException($"No values found in given dictionary for {recordName}", nameof(values));
             }
 
-            var json = File.ReadAllText(file.Path);
-            var values = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+            var propValues = values[recordName] as JObject;
+            if (propValues == null)
+            {
+                return null;
+            }
+
+            switch (propValues.Type)
+            {
+                case JTokenType.Object:
+                    builder.Append(GeneratePropertiesFromDict(propValues.ToObject<Dictionary<string, object>>()));
+                    break;
+            }
+
+            builder.AppendLine("    }");
+            builder.AppendLine("}");
+            return builder.ToString().Trim();
+        }
+
+        internal static string GeneratePropertiesFromDict(Dictionary<string, object> values)
+        {
+            var builder = new StringBuilder();
+            foreach (var kvp in values)
+            {
+                var declaration = GetPropertyTypeAndName(kvp.Key, kvp.Value);
+                builder.Append("        public ").Append(declaration).AppendLine(" { get; init; }");
+            }
+            return builder.ToString();
+        }
+
+        internal static List<string> ExtractTopLevelPropertyNames(Dictionary<string, object> values)
+        {
+
             if (values == null)
             {
-                continue;
+                return new List<string>();
             }
 
-            foreach (var recordName in ExtractTopLevelPropertyNames(values))
+            return values.Select(x => StringToValidRecordName(x.Key.Trim())).ToList();
+        }
+
+
+        internal static string GetPropertyTypeAndName(string fieldName, object fieldValue)
+        {
+            var typeName = "string";
+            switch (fieldValue)
             {
-                var source = GenerateRecordSource(recordName, values);
-                context.AddSource(recordName, SourceText.From(source, Encoding.UTF8));
+                case Boolean _:
+                    typeName = "bool";
+                    break;
+                case int _:
+                    typeName = "int";
+                    break;
+                case double _:
+                    typeName = "double";
+                    break;
+                case float _:
+                    typeName = "float";
+                    break;
+            };
+            return $"{typeName} {fieldName}";
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            foreach (var file in context.AdditionalFiles)
+            {
+                if (!Path.GetExtension(file.Path).Equals(".json", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+                var json = file.GetText()?.ToString();
+                var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                if (values == null)
+                {
+                    continue;
+                }
+
+                foreach (var recordName in ExtractTopLevelPropertyNames(values))
+                {
+                    var source = GenerateRecordSource(recordName, values);
+                    if (source == null)
+                    {
+                        continue;
+                    }
+
+                    context.AddSource(recordName, SourceText.From(source, Encoding.UTF8));
+                }
             }
         }
-    }
 
-    internal static string StringToValidRecordName(string s)
-    {
-        s = s.Trim();
-        s = char.IsLetter(s[0]) ? char.ToUpper(s[0]) + s.Substring(1) : s;
-        s = char.IsDigit(s.Trim()[0]) ? "_" + s : s;
-        s = new string(s.Select(ch => char.IsDigit(ch) || char.IsLetter(ch) ? ch : '_').ToArray());
-        return s;
-    }
+        internal static string StringToValidRecordName(string s)
+        {
+            s = s.Trim();
+            s = char.IsLetter(s[0]) ? char.ToUpper(s[0]) + s.Substring(1) : s;
+            s = char.IsDigit(s.Trim()[0]) ? "_" + s : s;
+            s = new string(s.Select(ch => char.IsDigit(ch) || char.IsLetter(ch) ? ch : '_').ToArray());
+            return s;
+        }
 
-    public void Initialize(GeneratorInitializationContext context)
-    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            //#if DEBUG
+            //            if (!Debugger.IsAttached)
+            //            {
+            //                Debugger.Launch();
+            //            }
+            //#endif
+        }
     }
 }
