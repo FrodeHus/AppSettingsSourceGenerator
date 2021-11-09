3db0e80d3d703800d561d52ea554e3a9b375184f
Frode Hus <frode.hus@outlook.com>
2021-11-09 13:18:10 +0100

    added demo project added code generation for simple appsettings

--- Reodor.AppSettingsSourceGenerator.Tests/AppSettingsTests.cs
+++ Reodor.AppSettingsSourceGenerator.Tests/AppSettingsTests.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using Xunit;
 
 namespace Reodor.AppSettingsSourceGenerator.Tests
@@ -9,16 +11,58 @@ namespace Reodor.AppSettingsSourceGenerator.Tests
         [Fact]
         public void It_Can_Extract_Classnames_Based_On_Property_Names()
         {
-            var source = @"
+            var json = @"
 {
   ""MySettings"": {
-    ""ApiEndpoint"": ""https://api.dockergen.frodehus.dev/""
+    ""Url"": ""https://api.dockergen.frodehus.dev/""
   }
 }
 ";
-            var classNames = AppSettingsSourceGenerator.ExtractTopLevelPropertyNames(source);
+            var values = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+
+            var classNames = AppSettingsSourceGenerator.ExtractTopLevelPropertyNames(values);
             classNames.Should().HaveCount(1);
             classNames.Single().Should().Be("MySettings");
+        }
+
+        [Theory]
+        [InlineData("test", "string")]
+        [InlineData(true, "bool")]
+        [InlineData(false, "bool")]
+        [InlineData(int.MaxValue, "int")]
+        [InlineData(double.MaxValue, "double")]
+        [InlineData(float.MaxValue, "float")]
+        public void It_Can_Create_Property_With_Proper_Type(object fieldValue, string expectedType)
+        {
+            var expected = $"{expectedType} Prop";
+            var actual = AppSettingsSourceGenerator.GetPropertyTypeAndName("Prop", fieldValue);
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void It_Can_Generate_Record_From_Json()
+        {
+            var json = @"
+{
+  ""MySettings"": {
+    ""Url"": ""http://localhost""
+  }
+}
+";
+            var expected = @"
+#nullable enable
+using System;
+namespace AppSettings;
+
+public record MySettings
+{
+    public string Url { get; init; }
+}
+";
+            var values = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+
+            var source = AppSettingsSourceGenerator.GenerateRecordSource("MySettings", values);
+            source.Should().Be(expected.Trim());
         }
     }
 }
--- Reodor.AppSettingsSourceGenerator/AppSettingsSourceGenerator.cs
+++ Reodor.AppSettingsSourceGenerator/AppSettingsSourceGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 using System.Text;
+using System.Text.Json;
 
 namespace Reodor.AppSettingsSourceGenerator;
 
@@ -7,41 +9,104 @@ namespace Reodor.AppSettingsSourceGenerator;
 public class AppSettingsSourceGenerator : ISourceGenerator
 {
 
-    internal string GenerateClassFile(string className)
+    internal static string GenerateRecordSource(string recordName, Dictionary<string, object> values)
     {
         var builder = new StringBuilder();
         builder.Append(@"
 #nullable enable
-namespace 
-");
+using System;
+namespace AppSettings;
 
+");
+        builder
+            .Append("public record ")
+            .AppendLine(recordName)
+            .Append('{')
+            .AppendLine();
+        if (values[recordName] == null || values[recordName] is not JsonElement)
+        {
+            throw new ArgumentException($"No values found in given dictionary for {recordName}", nameof(values));
+        }
+
+        var propValues = (JsonElement)values[recordName];
+        var properties = propValues.ValueKind switch
+        {
+            JsonValueKind.Object => GeneratePropertiesFromDict(propValues.Deserialize<Dictionary<string, object>>() ?? new Dictionary<string, object>()),
+            _ => null
+        };
+
+        if (properties != null)
+        {
+            builder.Append(properties);
+        }
+
+        builder.AppendLine("}");
+        return builder.ToString().Trim();
+    }
+
+    internal static string GeneratePropertiesFromDict(Dictionary<string, object> values)
+    {
+        var builder = new StringBuilder();
+        foreach (var kvp in values)
+        {
+            var declaration = GetPropertyTypeAndName(kvp.Key, kvp.Value);
+            builder.Append("    public ").Append(declaration).AppendLine(" { get; init; }");
+        }
         return builder.ToString();
     }
 
-    internal static List<string> ExtractTopLevelPropertyNames(string appsettingsJson)
+    internal static List<string> ExtractTopLevelPropertyNames(Dictionary<string, object> values)
     {
-        var values = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>(appsettingsJson);
 
         if (values == null)
         {
             return new List<string>();
         }
 
-        return values.Select(x => StringToValidClassName(x.Key)).ToList();
+        return values.Select(x => StringToValidRecordName(x.Key)).ToList();
     }
+
+
+    internal static string GetPropertyTypeAndName(string fieldName, object fieldValue)
+    {
+        var typeName = fieldValue switch
+        {
+            true => "bool",
+            false => "bool",
+            int _ => "int",
+            double _ => "double",
+            float _ => "float",
+            _ => "string"
+        };
+        return $"{typeName} {fieldName}";
+    }
+
     public void Execute(GeneratorExecutionContext context)
     {
         foreach (var file in context.AdditionalFiles)
         {
-            if (Path.GetExtension(file.Path).Equals(".json", StringComparison.OrdinalIgnoreCase)
-                && file.Path.StartsWith("appsettings", StringComparison.OrdinalIgnoreCase))
+            if (!Path.GetExtension(file.Path).Equals(".json", StringComparison.OrdinalIgnoreCase)
+                || !file.Path.StartsWith("appsettings", StringComparison.OrdinalIgnoreCase))
             {
+                continue;
+            }
 
+            var json = File.ReadAllText(file.Path);
+            var values = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+            if (values == null)
+            {
+                continue;
+            }
+
+            foreach (var recordName in ExtractTopLevelPropertyNames(values))
+            {
+                var source = GenerateRecordSource(recordName, values);
+                context.AddSource(recordName, SourceText.From(source, Encoding.UTF8));
             }
         }
     }
 
-    internal static string StringToValidClassName(string s)
+    internal static string StringToValidRecordName(string s)
     {
         s = s.Trim();
         s = char.IsLetter(s[0]) ? char.ToUpper(s[0]) + s.Substring(1) : s;
